3a908e4a5db0a401fffa8718914fc0609b01bfdd
ouharri <ouharrioutman@gmail.com>
2023-10-29 14:13:45 +0100

    setup the project

---
+++ src/test/java/org/marjane/MarjaneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marjane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarjaneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
