ea9b760de881e87db5319859d7fe5bd473ff62c1
ouharri <ouharrioutman@gmail.com>
2023-10-29 14:13:45 +0100

    setup the project

---
+++ src/main/java/org/marjane/MarjaneApplication.java
@@ -0,0 +1,13 @@
+package org.marjane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarjaneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarjaneApplication.class, args);
+    }
+
+}
