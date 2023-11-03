70e6f00f5776aff97f45b6b348f75ae65c0d309b
ouharri <ouharrioutman@gmail.com>
2023-11-03 11:09:23 +0100

    STRUCTURE DE MERDA

--- src/main/java/com/marjane/configuration/WebConfiguration.java
+++ src/main/java/com/marjane/configuration/WebConfiguration.java
@@ -1,16 +1,11 @@
 package com.marjane.configuration;
 
-import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
