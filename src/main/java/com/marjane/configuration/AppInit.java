f94f158eb3b12632adcb3b7646817a821c04f5b0
ouharri <ouharrioutman@gmail.com>
2023-11-06 13:49:43 +0100

    integration of the tomcat server into the core of the application

--- src/main/java/com/marjane/configuration/AppInit.java
+++ src/main/java/com/marjane/configuration/AppInit.java
@@ -1,16 +1,83 @@
 package com.marjane.configuration;
 
+import com.marjane.Core.dotenv;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.stereotype.Component;
 
-import jakarta.servlet.ServletContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.ContextLoaderListener;
+import java.io.File;
+import java.io.IOException;
 
-import jakarta.servlet.ServletRegistration;
-
+/**
+ * Cette classe gère l'initialisation et l'arrêt du serveur Tomcat pour l'application.
+ */
+@Slf4j
+@Component
 public class AppInit {
 
+    private static final int PORT = getPort();
+    final static Tomcat tomcat = new Tomcat();
 
+    /**
+     * Démarre le serveur Tomcat avec la configuration spécifiée.
+     */
+    public static void start() {
+        try {
+            tomcat.setBaseDir(createTempDir());
+            tomcat.setPort(PORT);
+            tomcat.getConnector();
+            tomcat.getHost().setAppBase(".");
+            tomcat.addWebapp("/", ".");
+            tomcat.start();
+            tomcat.getServer().await();
+        } catch (Exception e) {
+            log.error("Erreur lors du démarrage de Tomcat", e);
+        }
+    }
+
+    /**
+     * Arrête le serveur Tomcat en cours d'exécution.
+     */
+    public static void stop() {
+        try {
+            tomcat.stop();
+        } catch (Exception e) {
+            log.error("Erreur lors de l'arrêt de Tomcat", e);
+        }
+    }
+
+    /**
+     * Récupère le numéro de port à partir des variables d'environnement ou utilise un port par défaut.
+     *
+     * @return Le numéro de port du serveur Tomcat.
+     */
+    private static int getPort() {
+        String port = dotenv.get("SERVER_PORT");
+        if (port != null) {
+            try {
+                return Integer.parseInt(port);
+            } catch (NumberFormatException e) {
+                log.error("Format de SERVER_PORT invalide : " + port, e);
+            }
+        }
+        return 8080;
+    }
+
+    /**
+     * Crée un répertoire temporaire pour Tomcat.
+     *
+     * @return Le chemin absolu du répertoire temporaire.
+     */
+    private static String createTempDir() {
+        try {
+            File tempDir = File.createTempFile("tomcat.", "." + PORT);
+            tempDir.delete();
+            tempDir.mkdir();
+            tempDir.deleteOnExit();
+            return tempDir.getAbsolutePath();
+        } catch (IOException ex) {
+            log.error("Unable to create tempDir. java.io.tmpdir is set to " + System.getProperty("java.io.tmpdir"), ex);
+            throw new RuntimeException(ex);
+        }
+    }
 }
