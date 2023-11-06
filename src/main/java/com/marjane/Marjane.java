3c1f975422b53e09376a05603aba6a1164eb8831
ouharri <ouharrioutman@gmail.com>
2023-11-06 13:50:21 +0100

    the starting point of the entire application

--- src/main/java/com/marjane/Marjane.java
+++ src/main/java/com/marjane/Marjane.java
@@ -1,52 +1,9 @@
 package com.marjane;
 
-import com.marjane.Core.dotenv;
-import org.apache.catalina.startup.Tomcat;
-
-import java.io.File;
-import java.io.IOException;
+import com.marjane.configuration.AppInit;
 
 public class Marjane {
-    private static final int PORT = getPort();
-
-    public static void main(String[] args) throws Exception {
-        System.out.println(dotenv.get("SERVER_PORT"));
-        System.out.println(dotenv.get("JWT_SECRET"));
-
-        final Tomcat tomcat = new Tomcat();
-        tomcat.setBaseDir(createTempDir());
-        tomcat.setPort(PORT);
-        tomcat.getConnector();
-        tomcat.getHost().setAppBase(".");
-        tomcat.addWebapp("/", ".");
-        tomcat.start();
-        tomcat.getServer().await();
-    }
-
-    private static int getPort() {
-        String port = dotenv.get("SERVER_PORT");
-        if (port != null) {
-            return Integer.parseInt(port);
-        }
-        return 8080;
-    }
-
-    /**
-     * Returns the absolute temp dir for given servlet container.
-     *
-     * @return The temp dir for given servlet container.
-     */
-    private static String createTempDir() {
-        try {
-            File tempDir = File.createTempFile("tomcat.", "." + PORT);
-            tempDir.delete();
-            tempDir.mkdir();
-            tempDir.deleteOnExit();
-            return tempDir.getAbsolutePath();
-        } catch (IOException ex) {
-            throw new RuntimeException(
-                    "Unable to create tempDir. java.io.tmpdir is set to " + System.getProperty("java.io.tmpdir"), ex
-            );
-        }
+    public static void main(String[] args) {
+        AppInit.start();
     }
 }
