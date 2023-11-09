b96e779e566694cc36152300a3c540984f570228
ouharri <ouharrioutman@gmail.com>
2023-11-09 20:27:00 +0100

    dto for Authentification Entity

--- src/main/java/com/marjane/dto/AuthEntity.java
+++ src/main/java/com/marjane/dto/AuthEntity.java
@@ -4,10 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class AuthEntity {
     private String jwt;
-    private Integer userId;
+    private UUID userId;
 }
