828de95695c2e8c371337b9f97d90469e5b04e26
ouharri <ouharrioutman@gmail.com>
2023-11-10 19:35:05 +0100

    User Repository

--- src/main/java/com/marjane/Repositories/UserRepository.java
+++ src/main/java/com/marjane/Repositories/UserRepository.java
@@ -1,17 +1,16 @@
 package com.marjane.Repositories;
 
-import com.marjane.models.Person;
+import com.marjane.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<Person, UUID> {
-    Optional<Person> findByPersonalInfoEmail(String email);
-    Optional<Person> findByPersonalInfoPhoneNumber(String phoneNumber);
-    Optional<Person> findByPersonalInfoPhoneNumberOrPersonalInfoEmail(String phoneNumber, String email);
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByPersonalInfoEmail(String email);
+    Optional<User> findByPersonalInfoPhoneNumber(String phoneNumber);
+    Optional<User> findByPersonalInfoPhoneNumberOrPersonalInfoEmail(String phoneNumber, String email);
 
 }
