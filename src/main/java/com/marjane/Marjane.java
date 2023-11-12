99bac011835635f0980c135100ba7e32da4090c8
ouharri <ouharrioutman@gmail.com>
2023-11-12 13:10:45 +0100

    The main class of the Marjane application responsible for starting the server.

--- src/main/java/com/marjane/Marjane.java
+++ src/main/java/com/marjane/Marjane.java
@@ -2,7 +2,16 @@ package com.marjane;
 
 import com.marjane.core.Application;
 
+/**
+ * The main class of the Marjane application responsible for starting the Tomcat server.
+ */
 public class Marjane {
+
+    /**
+     * The main method that launches the application by calling the static start method of the Application class.
+     *
+     * @param args Command line arguments (not used in this application).
+     */
     public static void main(String[] args) {
         Application.start();
     }
